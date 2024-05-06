a1630571c2db9797b0659b0b2900c0d4b344f641
noelkirui1 <kiruinoel16@gmail.com>
2024-05-06 16:26:48 +0300

    commit

--- components/App.js
+++ components/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Header from "./Header";
+import About from "./About";
 import NavBar from "../../react-hooks-react-router-routes-lab-v6/src/components/NavBar";
 
 function App() {
