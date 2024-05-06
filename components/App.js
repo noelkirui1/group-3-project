86a774518d1845e21c634bced70ba4249f91bb0a
noelkirui1 <kiruinoel16@gmail.com>
2024-05-06 15:03:57 +0300

    commit

---
+++ components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../../react-hooks-react-router-routes-lab-v6/src/components/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <About />
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
